9a2c2bb3fa8bde78f648e4b9c69d383ade7bca44
Holt Skinner <HoltWasHere@gmail.com>
2017-11-05 17:41:59 -0600

    Added Baby's first test, and text examples

--- hosna_homework3.hs
+++ hosna_homework3.hs
@@ -1,13 +1,13 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
 {-# OPTIONS_GHC -fno-warn-tabs #-}
-module Homework3 where
 
+module Homework3 where
 import Test.Hspec
 import RPNAST
 
 
-prob1    :: a
-prob1    = undefined
+prob1 :: String -> PExp
+prob1 _ = []
 
 prob2    :: a
 prob2    = undefined
@@ -19,3 +19,14 @@ prob4    :: a
 prob4    = undefined
 
 -- Write your Hspec Tests below
+test_prob1 :: IO ()
+
+test_prob1 = hspec $ do
+  describe "Prob1 from HW3" $ do
+    context "For empty string" $ do
+      it "should return the empty list" $ do
+        prob1 "" `shouldBe` []
+
+    context "For \"200 + - * /\"" $ do
+      it "should return \'[Val 200, Plus, Minus, Mul, IntDiv]\'" $ do
+        prob1 "200 + - * /" `shouldBe` [Val 200, Plus, Minus, Mul, IntDiv]
